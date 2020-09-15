9250c06c361b63d3daebcc29ca7c87d52ffa0be7
chibuisi <enyia21@gmail.com>
2020-09-15 12:20:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     console.log("The DOM has loaded");
+//   });
+
+//   console.log(
+//     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+//   );
+
+// I believe that this is the call back function:
+document.addEventListener('DOMContentLoaded', () => {
+    theDomHasLoaded();
+    })
+
+// document.addEventListener("DOMContentLoaded", theDomHasLoaded, false);
+
+function theDomHasLoaded() {
+    document.getElementById('text').innerHTML = "This is really cool!";;
+    // domTestOnLoadChange.innerHTML = "This is really cool!";
+}
